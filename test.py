1c6f8fdab13d22474120900e26fdb67ce33d01a4
fabian-fony-code <fabiggggaray@gmail.com>
2026-04-25 10:21:45 -0300

    add test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+#edad = int(input("Escribe tu edad determinada: "))
+
+#if edad >= 18:
+#    print("¡Eres mayor de edad!")
+#else:
+#    print("¡Eres menor de edad!")
+
+#temperatura = float(input("Ingrese la temperatura: "))
+
+#if temperatura > 20:
+#    print("Hace calor afuera...")
+#elif temperatura >=15:
+#    print("El clima es templado...")
+#else:
+#    print ("Que frio hace afuera...")
+
